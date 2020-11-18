22064c102306bc0f906250c34bb81bb5e55694c2
Iswaryalashmi Narayanan <69483035+isunanana@users.noreply.github.com>
2020-11-18 14:52:50 -0600

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,27 @@
+package com.example.getmehired;
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        int fact =1;
+
+        // Create scanner class object
+        Scanner in = new Scanner(System.in);
+
+
+        // Input a number from user to calculate factorial
+        System.out.println("Enter number :");
+        int num = in.nextInt();
+
+
+        if (num < 0)
+            System.out.println("Number should be non-negative.");
+        else {
+            for (int i = 1; i <= num; i++)
+                fact = fact * i;
+
+            System.out.println("Factorial of " + num + " is = " + fact6);
+        }
+
+    }
+}
